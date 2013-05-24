a6baac976d64f3bdb0dacc533210c84dc16c45da
Alain <alain@Alains-MacBook-Pro-3.local>
2013-05-24 19:11:07 +0200

    Initial Commit

---
+++ Lights/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lights
+//
+//  Created by Alain on 24.05.13.
+//  Copyright (c) 2013 Zone Zero Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lights/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lights
+//
+//  Created by Alain on 24.05.13.
+//  Copyright (c) 2013 Zone Zero Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
